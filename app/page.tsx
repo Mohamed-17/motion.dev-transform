be8653ba0fc9cab2b4204db337b18eba6e26d6a3
Mohamed ashraf <muhamed77700m@gmail.com>
2025-10-14 06:50:53 +0300

    transitions

--- app/page.tsx
+++ app/page.tsx
@@ -3,20 +3,11 @@ import * as motion from "motion/react-client";
 function HomePage() {
   return (
     <div className="space-y-5">
-      <motion.div animate={{ x: 100 }} className="box"></motion.div>
-      <motion.div animate={{ y: 100 }} className="box"></motion.div>
-      <motion.div className="box" animate={{ scaleX: 2 }} />
-      <motion.div className="box" animate={{ scaleY: 2 }} />
       <motion.div
+        animate={{ x: 100, y: 100 }}
+        transition={{ duration: 1, ease: "easeIn", delay: 2 }}
         className="box"
-        animate={{ skewX: 60, skewY: 10 }}
       ></motion.div>
-      <motion.div
-        className="box"
-        animate={{
-          rotate: 60,
-        }}
-      />
     </div>
   );
 }
