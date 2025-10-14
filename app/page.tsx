053f7cf6d24c373ffb2c0852080d0923c36533ef
Mohamed ashraf <muhamed77700m@gmail.com>
2025-10-14 15:33:24 +0300

    staggerChildren

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,54 @@
-import React from "react";
-import * as motion from "motion/react-client";
+"use client";
+import React, { useState } from "react";
+import { motion } from "motion/react";
 function HomePage() {
-  const boxVariants = {
-    init: { opacity: 1, scale: 1 },
-    hide: { opacity: 0.5, scale: 0.8 },
-    click: { opacity: 0, scale: 0 },
+  const [isVisible, setIsvisible] = useState(false);
+  const parentVariants = {
+    hidden: {
+      opacity: 0,
+    },
+    visible: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.5,
+      },
+    },
+  };
+  const childVariants = {
+    hidden: {
+      opacity: 0,
+      y: 20,
+    },
+    visible: {
+      opacity: 1,
+      y: 0,
+    },
   };
   return (
-    <div className="space-x-3 flex">
+    <div>
       <motion.div
-        variants={boxVariants}
-        className="w-80 h-80 bg-blue-400 rounded-lg"
-        initial={"init"}
-        whileHover={"hide"}
-      ></motion.div>
+        className="flex space-x-3"
+        variants={parentVariants}
+        initial="hidden"
+        animate={isVisible ? "visible" : "hidden"}
+      >
+        {[...Array(5)].map((_, index) => (
+          <motion.div
+            key={index}
+            className="box"
+            variants={childVariants}
+          ></motion.div>
+        ))}
+      </motion.div>
+
+      <div className="mt-20 flex justify-center">
+        <button
+          onClick={() => setIsvisible((prev) => !prev)}
+          className="bg-white text-black font-bold rounded px-5 py-2"
+        >
+          {isVisible ? "Hide Element" : "Show Element"}
+        </button>
+      </div>
     </div>
   );
 }
